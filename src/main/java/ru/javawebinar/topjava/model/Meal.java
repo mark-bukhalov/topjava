96b20703f420f6446394e8a4878d4e5afd8627c4
mark-bukhalov <mark.bukhalov98@gmail.com>
2024-05-22 17:09:56 +0300

    add user to meal

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -4,8 +4,9 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
-public class Meal {
-    private Integer id;
+public class Meal extends AbstractBaseEntity {
+
+    private final Integer userID;
 
     private final LocalDateTime dateTime;
 
@@ -13,23 +14,20 @@ public class Meal {
 
     private final int calories;
 
-    public Meal(LocalDateTime dateTime, String description, int calories) {
-        this(null, dateTime, description, calories);
+    public Meal(Integer userID, LocalDateTime dateTime, String description, int calories) {
+        this(null, userID, dateTime, description, calories);
     }
 
-    public Meal(Integer id, LocalDateTime dateTime, String description, int calories) {
-        this.id = id;
+    public Meal(Integer id, Integer userID, LocalDateTime dateTime, String description, int calories) {
+        super(id);
+        this.userID = userID;
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
     }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
+    public Integer getUserID() {
+        return userID;
     }
 
     public LocalDateTime getDateTime() {
@@ -52,10 +50,6 @@ public class Meal {
         return dateTime.toLocalTime();
     }
 
-    public boolean isNew() {
-        return id == null;
-    }
-
     @Override
     public String toString() {
         return "Meal{" +
@@ -66,3 +60,4 @@ public class Meal {
                 '}';
     }
 }
+
--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -4,10 +4,12 @@ import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
 
-import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class InMemoryMealRepository implements MealRepository {
     private final Map<Integer, Meal> repository = new ConcurrentHashMap<>();
@@ -25,7 +27,7 @@ public class InMemoryMealRepository implements MealRepository {
             return meal;
         }
         // handle case: update, but not present in storage
-        return repository.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+        return repository.computeIfPresent(meal.getId(), (id, oldUser) -> meal);
     }
 
     @Override
@@ -39,8 +41,23 @@ public class InMemoryMealRepository implements MealRepository {
     }
 
     @Override
-    public Collection<Meal> getAll() {
-        return repository.values();
+    public List<Meal> getAll() {
+        List<Meal> meals = (List<Meal>) repository.values();
+        return sortMealByDateTimeDesc(meals);
     }
+
+    @Override
+    public List<Meal> getAllByUser(Integer userId) {
+        List<Meal> meals = repository.values().stream()
+                .filter(value -> value.getUserID().equals(userId))
+                .collect(Collectors.toList());
+        return sortMealByDateTimeDesc(meals);
+    }
+
+    private List<Meal> sortMealByDateTimeDesc(List<Meal> meals) {
+        meals.sort(Comparator.comparing(Meal::getDateTime).reversed());
+        return meals;
+    }
+
 }
 

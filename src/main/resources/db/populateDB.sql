a9ac3387b6f18ba74bdaa1789fb21c30f8558b0c
mark-bukhalov <mark.bukhalov98@gmail.com>
2024-05-30 17:13:33 +0300

    create and init meals in database

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,9 +1,15 @@
 DELETE
 FROM user_role
 WHERE user_id is not null;
+
 DELETE
 FROM users
 where id is not null;
+
+DELETE
+FROM meals
+where id is not null;
+
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -14,3 +20,12 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, date_time, description, calories)
+VALUES (100000, '2020-01-30 10:00:00', 'Завтрак', 500),
+       (100000, '2020-01-30 13:00:00', 'Обед', 1000),
+       (100000, '2020-01-30 20:00:00', 'Ужин', 500),
+       (100001, '2020-01-31 00:00:00', 'Еда на граничное значение', 100),
+       (100001, '2020-01-31 10:00:00', 'Завтрак', 1000),
+       (100001, '2020-01-31 13:00:00', 'Обед', 500),
+       (100001, '2020-01-31 20:00:00', 'Ужин', 410);

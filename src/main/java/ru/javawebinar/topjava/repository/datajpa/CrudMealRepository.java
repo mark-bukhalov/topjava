40f4abd8e50dc0dab28505ef4ce2f6db47539b58
mark-bukhalov <mark.bukhalov98@gmail.com>
2024-06-07 15:54:19 +0300

    imp DataJpaMealRepository

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -4,12 +4,15 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
     Meal getByIdAndUserId(int id, int userId);
 
-    List<Meal> deleteMealByIdAndUserId(int id, int userId);
+    int deleteMealByIdAndUserId(int id, int userId);
 
-    List<Meal> getAllByUserID(int userId, Sort sort);
+    List<Meal> getAllByUserId(int userId, Sort sort);
+
+    List<Meal> getAllByUserIdAndDateTimeGreaterThanEqualAndDateTimeLessThan(int userId, LocalDateTime startDateTime, LocalDateTime endDateTime, Sort sort);
 }
--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -4,29 +4,41 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
+@Transactional(readOnly = true)
 public class DataJpaMealRepository implements MealRepository {
-    private static final Sort SORT_DATETIME = Sort.by(Sort.Direction.ASC, "date_time");
+    private static final Sort SORT_DATETIME = Sort.by(Sort.Direction.DESC, "dateTime");
     private final CrudMealRepository crudRepository;
+    @PersistenceContext
+    private EntityManager em;
 
     public DataJpaMealRepository(CrudMealRepository crudRepository) {
         this.crudRepository = crudRepository;
     }
 
     @Override
+    @Transactional
     public Meal save(Meal meal, int userId) {
-        return null;
+        if (meal.isNew() || get(meal.getId(), userId) != null) {
+            meal.setUser(em.getReference(User.class, userId));
+            return crudRepository.save(meal);
+        } else {
+            return null;
+        }
     }
 
     @Override
     @Transactional //https://stackoverflow.com/a/58861890
     public boolean delete(int id, int userId) {
-        return crudRepository.deleteMealByIdAndUserId(id, userId) != null;
+        return crudRepository.deleteMealByIdAndUserId(id, userId) != 0;
     }
 
     @Override
@@ -36,11 +48,11 @@ public class DataJpaMealRepository implements MealRepository {
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return crudRepository.getAllByUserId(userId, SORT_DATETIME);
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+        return crudRepository.getAllByUserIdAndDateTimeGreaterThanEqualAndDateTimeLessThan(userId, startDateTime, endDateTime, SORT_DATETIME);
     }
 }

1fe0d51706bde5ab2cb82a1b9f8d1ffea129af7e
mark-bukhalov <mark.bukhalov98@gmail.com>
2024-07-09 13:49:13 +0300

    fix tests

--- src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaUserServiceTest.java
@@ -2,13 +2,11 @@ package ru.javawebinar.topjava.service.datajpa;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.MealTestData;
 import ru.javawebinar.topjava.UserTestData;
 import ru.javawebinar.topjava.model.User;
-import ru.javawebinar.topjava.repository.JpaUtil;
-import ru.javawebinar.topjava.service.AbstractUserServiceTest;
+import ru.javawebinar.topjava.service.AbstractJpaUserServiceTest;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import static ru.javawebinar.topjava.MealTestData.MEAL_MATCHER;
@@ -16,16 +14,7 @@ import static ru.javawebinar.topjava.Profiles.DATAJPA;
 import static ru.javawebinar.topjava.UserTestData.*;
 
 @ActiveProfiles(DATAJPA)
-public class DataJpaUserServiceTest extends AbstractUserServiceTest {
-
-    @Autowired
-    protected JpaUtil jpaUtil;
-
-    @Override
-    public void setup() {
-        super.setup();
-        jpaUtil.clear2ndLevelHibernateCache();
-    }
+public class DataJpaUserServiceTest extends AbstractJpaUserServiceTest {
 
     @Test
     public void getWithMeals() {
--- src/test/java/ru/javawebinar/topjava/service/jpa/JpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jpa/JpaUserServiceTest.java
@@ -3,12 +3,12 @@ package ru.javawebinar.topjava.service.jpa;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.repository.JpaUtil;
-import ru.javawebinar.topjava.service.AbstractUserServiceTest;
+import ru.javawebinar.topjava.service.AbstractJpaUserServiceTest;
 
 import static ru.javawebinar.topjava.Profiles.JPA;
 
 @ActiveProfiles(JPA)
-public class JpaUserServiceTest extends AbstractUserServiceTest {
+public class JpaUserServiceTest extends AbstractJpaUserServiceTest {
 
     @Autowired
     protected JpaUtil jpaUtil;

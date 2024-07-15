dfbef19f749bf6decc02e76c6ed5c3bfc51a8eaa
mark-bukhalov <mark.bukhalov98@gmail.com>
2024-07-15 17:46:07 +0300

    test style.css

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -49,4 +49,12 @@ class RootControllerTest extends AbstractControllerTest {
                         }
                 ));
     }
+
+    @Test
+    void getStyle() throws Exception {
+        perform(get("/resources/css/style.css"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/css;charset=UTF-8"));
+    }
 }

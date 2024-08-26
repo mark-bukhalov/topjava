e78c0326c7581c336ca75e1916698aa07b10b751
mbuhalov <mark.bukhalov98@gmail.com>
2024-08-26 21:34:45 +0300

    add lang

---
+++ src/main/java/ru/javawebinar/topjava/util/MessageUtil.java
@@ -0,0 +1,8 @@
+package ru.javawebinar.topjava.util;
+
+public class MessageUtil {
+    public static final String ERR_DUP_DATETIME = "err.dupDateTime";
+    public static final String PASS_LENGHT = "err.passLenght";
+    public static final String DUP_EMAIL = "err.dupEmail";
+
+}
